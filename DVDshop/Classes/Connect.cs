5b0915b8dfcb425f35bfd27860473637f115629f
kaneki it <laborotorydeath@yandex.ru>
2023-12-15 09:48:01 +0300

    gg

---
+++ DVDshop/Classes/Connect.cs
@@ -0,0 +1,14 @@
+﻿using DVDshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDShop.Classes
+{
+    class Connect
+    {
+        public static DVDDVDEntities modeldb;
+    }
+}
